d613f41aeef6786c920fb6fde17959490b15ce19
TheLosst <79166172444@yandex.ru>
2023-12-19 10:21:59 +0300

    Кое-как адаптированны основные страницы

--- lib/Pages/MeetMeEditProfile.dart
+++ lib/Pages/MeetMeEditProfile.dart
@@ -77,187 +77,124 @@ class _EditProfilePageState extends State<EditProfilePage> {
     }
 
     return Scaffold(
-      body: Center(
-        child: FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Stack(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 1080 * 0.07, left: 1920 * 0.08),
-                  child: Text(
-                    "Моя анкета",
-                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 1080 * 0.16, left: 1920 * 0.08),
-                  child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Container(
-                      width: 480,
-                      height: 620,
-                      foregroundDecoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            userLoggined.linkToIMG,
-                          ),
-                          fit: BoxFit.fill
-                        )
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Моя анкета",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+        ),
+        backgroundColor: Color.fromRGBO(228, 122, 167, 1),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              FittedBox(
+                fit: BoxFit.contain,
+                child: Container(
+                  width: 480,
+                  height: 620,
+                  foregroundDecoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        userLoggined.linkToIMG,
                       ),
-                    ),
-                    // child: Image.network(
-                    //   userLoggined.linkToIMG,
-                    //   width: 416,
-                    //   height: 664,
-                    // ),
+                      fit: BoxFit.fill
+                    )
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 1080 * 0.18, left: 1920 * 0.37),
-                  child: Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            //InputDatePickerFormField(firstDate: firstDate, lastDate: lastDate)
-                            Container(
-                                width: 500,
-                                height: 60,
-                                child: TextFormField(
-                                  controller: myName,
-                                  maxLength: 20,
-                                )),
-                            SizedBox(
-                              height: 42,
-                              width: 1,
-                            ),
-                            Container(
-                                width: 500,
-                                height: 60,
-                                child: TextFormField(
-                                  controller: age,
-                                  maxLength: 32,
-                                  keyboardType: TextInputType.number,
-                                  inputFormatters: [
-                                    FilteringTextInputFormatter.allow(RegExp(
-                                        '\\d{0,2}\.\?\\d{0,2}\.\\d{0,4}')),
-                                    DateTextFormatter(),
-                                  ],
-                                )),
-                            SizedBox(
-                              height: 11,
-                              width: 1,
-                            ),
-                            Text(
-                              "Регион: Россия, Москва",
-                            ),
-                            SizedBox(
-                              height: 11,
-                              width: 1,
-                            ),
-                            Container(
-                                width: 500,
-                                height: 60,
-                                child: TextFormField(
-                                  controller: myInterest,
-                                  maxLength: 32,
-                                )),
-                            SizedBox(
-                              height: 11,
-                              width: 1,
-                            ),
-                            Container(
-                                width: 500,
-                                height: 60,
-                                child: TextFormField(
-                                  controller: meetingAge,
-                                  maxLength: 32,
-                                )),
-                            SizedBox(
-                              height: 11,
-                              width: 1,
-                            ),
-                            Container(
-                                width: 500,
-                                height: 60,
-                                child: TextFormField(
-                                  controller: targetMeet,
-                                  maxLength: 32,
-                                )),
-                            SizedBox(
-                              height: 11,
-                              width: 1,
-                            ),
-                            Container(
-                                width: 500,
-                                height: 60,
-                                child: TextFormField(
-                                  controller: aboutUsr,
-                                  maxLength: 999,
-                                )),
-                            SizedBox(
-                              height: 11,
-                              width: 1,
-                            ),
-                            Container(
-                                width: 500,
-                                height: 60,
-                                child: TextFormField(
-                                  controller: linkToIMG,
-                                  maxLength: 9999,
-                                )),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+                // child: Image.network(
+                //   userLoggined.linkToIMG,
+                //   width: 416,
+                //   height: 664,
+                // ),
+              ),
+              TextFormField(
+                controller: myName,
+                maxLength: 20,
+              ),
+              TextFormField(
+                controller: age,
+                maxLength: 32,
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(
+                      '\\d{0,2}\.\?\\d{0,2}\.\\d{0,4}')),
+                  DateTextFormatter(),
+                ],
+              ),
+              Text(
+                "Регион: Россия, Москва",
+              ),
+              SizedBox(
+                height: 11,
+                width: 1,
+              ),
+              TextFormField(
+                controller: myInterest,
+                maxLength: 32,
+              ),
+              TextFormField(
+                controller: meetingAge,
+                maxLength: 32,
+              ),
+              SizedBox(
+                height: 11,
+                width: 1,
+              ),
+              TextFormField(
+                controller: targetMeet,
+                maxLength: 32,
+              ),
+              SizedBox(
+                height: 11,
+                width: 1,
+              ),
+              TextFormField(
+                controller: aboutUsr,
+                maxLength: 999,
+              ),
+              SizedBox(
+                height: 11,
+                width: 1,
+              ),
+              TextFormField(
+                controller: linkToIMG,
+                maxLength: 9999,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  userLoggined.username = myName.text.toString().replaceFirst("Имя: ", "");
+                  userLoggined.birthDay = age.text.toString();
+                  userLoggined.withMeets = myInterest.text.toString().replaceFirst("Меня интересует: ", "");
+                  userLoggined.targetMeet = targetMeet.text.toString().replaceFirst("Цель: ", "");
+                  userLoggined.aboutUser = aboutUsr.text.toString().replaceFirst("Описание: ", "");
+                  userLoggined.linkToIMG = linkToIMG.text;
+                  userLoggined.getAll();
+                  editAndGetProfile();
+                  Push().PushTo(Profile(), context);
+                },
+                child: Text(
+                  "Сохранить изменения",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 847, left: 1451),
-                  child: Column(
-                    children: [
-                      Container(
-                          width: 363,
-                          height: 80,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              userLoggined.username = myName.text.toString().replaceFirst("Имя: ", "");
-                              userLoggined.birthDay = age.text.toString();
-                              userLoggined.withMeets = myInterest.text.toString().replaceFirst("Меня интересует: ", "");
-                              userLoggined.targetMeet = targetMeet.text.toString().replaceFirst("Цель: ", "");
-                              userLoggined.aboutUser = aboutUsr.text.toString().replaceFirst("Описание: ", "");
-                              userLoggined.linkToIMG = linkToIMG.text;
-                              userLoggined.getAll();
-                              editAndGetProfile();
-                              Push().PushTo(Profile(), context);
-                            },
-                            child: Text(
-                              "Сохранить изменения",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateColor.resolveWith(
-                                    (states) =>
-                                        Color.fromRGBO(255, 239, 246, 1)),
-                                shape: MaterialStateProperty.all<
-                                        RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(15),
-                                        side:
-                                            BorderSide(color: Colors.white)))),
-                          )),
-                    ],
-                  ),
-                )
-              ],
-            )),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateColor.resolveWith(
+                        (states) =>
+                            Color.fromRGBO(255, 239, 246, 1)),
+                    shape: MaterialStateProperty.all<
+                            RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            side:
+                                BorderSide(color: Colors.white)))),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
